803d602c823e8ac248f37ab1754a54379e67d78a
peppeluc22 <gluca0260@gmail.com>
2023-06-15 14:54:49 +0200

    added character cards and page navigation

---
+++ src/components/CharactersList.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <CharacterCard v-for="character in store.characters" :key="character.id" :character="character" />
+        </div>
+
+        <!-- Page navigation -->
+        <ul class="pagination">
+            <li class="page-item"><button :class="{ 'page-link': true, 'disabled': store.currentPage === 1 }"
+                    @click="getCharacter(store.currentPage - 1)">Previous</button></li>
+            <li class="page-item" v-for="n in store.lastPage"><button
+                    :class="{ 'page-link': true, 'active': store.currentPage === n }" @click="getCharacter(n)">{{ n
+                    }}</button>
+            </li>
+
+            <li class="page-item"><button :class="{ 'page-link': true, 'disabled': store.currentPage === store.lastPage }"
+                    @click="getCharacter(store.currentPage + 1)">Next</button></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { store } from '../data/store';
+import CharacterCard from '../components/CharacterCard.vue';
+import axios from 'axios';
+export default {
+    name: 'CharactersList',
+    components: {
+        CharacterCard
+    },
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        getCharacter(numPage) {
+            axios.get(`${store.apiUrl}/characters`, {
+                params: {
+                    'page': numPage
+                }
+            }).then((res) => {
+                store.characters = res.data.results.data
+                store.currentPage = res.data.results.current_page
+                store.lastPage = res.data.results.last_page
+            })
+        }
+    },
+    mounted() {
+        this.getCharacter(1);
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
