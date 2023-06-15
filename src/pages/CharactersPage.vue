8b2d37f60488d3110582da81bb0aa6a0d553b44f
peppeluc22 <gluca0260@gmail.com>
2023-06-15 14:56:06 +0200

    added characterlist to characterpage

---
+++ src/pages/CharactersPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <CharactersList />
+    </div>
+</template>
+
+<script>
+import CharactersList from '../components/CharactersList.vue';
+export default {
+    name: 'CharactersPage',
+    components: {
+        CharactersList
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
