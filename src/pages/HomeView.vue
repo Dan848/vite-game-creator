8017b382d138e1111348429500d6d895e599a72f
peppeluc22 <gluca0260@gmail.com>
2023-06-15 20:20:07 +0200

    added section component and removed old sections

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -4,58 +4,30 @@
       Official Game!</h1>
   </div>
 
-  <!-- Choose your character section -->
-  <section id="choose-your-character" class="container-fluid">
-    <div class="container">
-      <div class="row mt-5">
-        <div class="col-6">
-          <img src="https://i.pinimg.com/originals/86/4b/1e/864b1ec73be1e41d7e16d0a43434cdfb.gif" alt="figth-gif"
-            class="w-100 h-100">
-        </div>
+  <!-- Fight section -->
+  <HomeSection :section="store.FightSection" />
 
-        <div class="col-6">
-          <div>
-            <h2>Scegli il tuo personaggio e inizia a <span class="text-danger">combattere!</span></h2>
-          </div>
-          <div><span class="fs-3">Puoi scegliere il tuo personaggio</span> <router-link :to="{ name: 'characters' }"
-              class="btn text-uppercase fw-bold btn-primary">qui</router-link> </div>
-        </div>
-      </div>
-    </div>
-  </section>
 
-  <!-- Alliance section -->
-  <section id="alliance" class="container-fluid">
-    <div class="container">
-      <div class="row mt-5">
-        <div class="col-6">
-          <div>
-            <h2>Oppure chiama i tuoi amici e <span class="text-warning">alleati</span></h2>
-          </div>
-          <p class="lh-lg">L'alleanza è resa stabile da riti, che comprendono generalmente il <span
-              class="text-danger">sacrificio</span> comune (seguito
-            o meno da un
-            vero e proprio passo sacrificale e dal giuramento che invoca l’ira divina su chi lo violi) o
-            qualunque altro
-            gesto che significhi l’instaurarsi di un legame (per es. fumare il 'calumet della pace' presso gli Indiani
-            dell’America Settentrionale). Tanto più sacro del patto che stabilisce la
-            pace o una comunanza d’intenti tra
-            società umane, è quello che assicura a una comunità la pace con la divinità, quella che i Romani chiamavano
-            la
-            pax deorum. </p>
-        </div>
+  <!-- Ally section -->
+  <HomeSection :section="store.AllySection" />
 
-        <div class="col-6">
-          <img src="https://i.gifer.com/N2AL.gif" alt="ally-gif" class="w-100 h-100">
-        </div>
-      </div>
-    </div>
-  </section>
+  <!-- Have fun section -->
+  <HomeSection :section="store.HaveFunSection" />
 </template>
 
 <script>
+import { store } from '../data/store';
+import HomeSection from '../components/HomeSection.vue';
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: {
+    HomeSection
+  },
+  data() {
+    return {
+      store
+    }
+  }
 }
 </script>
 
