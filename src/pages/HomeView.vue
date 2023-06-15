e7426207279b71b6f63a4bb3ace96be819ce498d
peppeluc22 <gluca0260@gmail.com>
2023-06-15 17:04:58 +0200

    fixed sections

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,33 +1,37 @@
 <template>
-  <div class="container mt-5">
-    <div>
-      <h1 id="home-title">Benvenuto su <span class="bugText px-3">Bug</span><span class="makersText px-3">Makers</span>
-        Official Game!</h1>
-    </div>
+  <div class="text-center mt-5">
+    <h1 id="home-title">Benvenuto su <span class="bugText px-3">Bug</span><span class="makersText px-3">Makers</span>
+      Official Game!</h1>
+  </div>
 
-    <section id="choose-your-character">
-      <div>
-        <h2>Scegli il tuo personaggio e inizia a <span class="text-danger">combattere!</span></h2>
-      </div>
+  <!-- Choose your character section -->
+  <section id="choose-your-character" class="container-fluid">
+    <div class="container">
       <div class="row mt-5">
         <div class="col-6">
           <img src="https://i.pinimg.com/originals/86/4b/1e/864b1ec73be1e41d7e16d0a43434cdfb.gif" alt="figth-gif"
             class="w-100 h-100">
         </div>
 
-        <div class="col-6 d-flex align-items-center">
+        <div class="col-6">
+          <div>
+            <h2>Scegli il tuo personaggio e inizia a <span class="text-danger">combattere!</span></h2>
+          </div>
           <div><span class="fs-3">Puoi scegliere il tuo personaggio</span> <router-link :to="{ name: 'characters' }"
               class="btn text-uppercase fw-bold btn-primary">qui</router-link> </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <section id="alliance">
-      <div>
-        <h2>Oppure chiama i tuoi amici e <span class="text-warning">alleati</span></h2>
-      </div>
+  <!-- Alliance section -->
+  <section id="alliance" class="container-fluid">
+    <div class="container">
       <div class="row mt-5">
         <div class="col-6">
+          <div>
+            <h2>Oppure chiama i tuoi amici e <span class="text-warning">alleati</span></h2>
+          </div>
           <p class="lh-lg">L'alleanza è resa stabile da riti, che comprendono generalmente il <span
               class="text-danger">sacrificio</span> comune (seguito
             o meno da un
@@ -36,36 +40,17 @@
             gesto che significhi l’instaurarsi di un legame (per es. fumare il 'calumet della pace' presso gli Indiani
             dell’America Settentrionale). Tanto più sacro del patto che stabilisce la
             pace o una comunanza d’intenti tra
-            società umane, è quello che assicura a una comunità la pace con la divinità, quella che i Romani chiamavano la
+            società umane, è quello che assicura a una comunità la pace con la divinità, quella che i Romani chiamavano
+            la
             pax deorum. </p>
         </div>
 
         <div class="col-6">
           <img src="https://i.gifer.com/N2AL.gif" alt="ally-gif" class="w-100 h-100">
         </div>
-
-
       </div>
-    </section>
-
-    <section id="have-fun">
-      <div>
-        <h2>L'importante rimane però sempre <span class="text-secondary">divertirsi!</span></h2>
-      </div>
-      <div class="row mt-5">
-        <div class="col-6">
-          <img src="https://media4.giphy.com/media/kHmVOy84g8G6my09fu/giphy.gif" alt="ally-gif" class="w-100 h-100">
-        </div>
-
-        <div class="col-6">
-          <h3 class="fw-bold">Ti raccomandiamo di leggere il regolamento prima di giocare!</h3>
-
-          <h4 class="mt-5">Adesso vai e divertiti avventuriero</h4>
-          <div><button class="text-uppercase fw-semibold btn text-bg-secondary">Play</button></div>
-        </div>
-      </div>
-    </section>
-  </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -95,5 +80,10 @@ export default {
 
 section {
   margin-top: 6rem;
+  padding: 2rem;
+}
+
+#alliance {
+  background-color: #0000000c;
 }
 </style>
