708fe74ef1a4b4710d1842aad5f63c8843d52e4d
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-06-19 14:29:01 +0200

    add form + button

--- src/pages/ContactsView.vue
+++ src/pages/ContactsView.vue
@@ -1,15 +1,39 @@
 <template>
-    <div>
-
+  <div class="container">
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label"
+        >Email address</label
+      >
+      <input
+        type="email"
+        class="form-control"
+        id="exampleFormControlInput1"
+        placeholder="Inserisci la tua email"
+      />
     </div>
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label"
+        >Example textarea</label
+      >
+      <textarea
+        class="form-control"
+        id="exampleFormControlTextarea1"
+        rows="3"
+      ></textarea>
+    </div>
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+      <button class="btn bm-btn me-md-2" type="reset">Cancella</button>
+      <button class="btn bm-btn" type="submit">Invia</button>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'ContactsView'
-    }
+export default {
+  name: "ContactsView",
+};
 </script>
 
 <style lang="scss" scoped>
-
+@use "../assets/partials/var.scss" as *;
 </style>
