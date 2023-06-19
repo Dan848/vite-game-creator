7ecab6637fcf683c8883d55208b8696bda4a7af9
peppeluc22 <gluca0260@gmail.com>
2023-06-19 14:33:23 +0200

    fixed page title

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
-    <h2 class="text-center mt-2 pixel-text text-secondary">Conosci meglio il nostro Team!</h2>
+    <h2 class="text-center my-5 pixel-text text-secondary">Conosci meglio il nostro Team!</h2>
     <div class="row align-items-stretch">
       <AboutUsCard v-for="team in store.Teams" :key="team.id" :team="team" />
-      
+
     </div>
     <div class="position-relative text-secondary my-5 mx-auto guest">
       <div class="card p-3">
@@ -15,8 +15,8 @@
         <br>
         <h5>Thanks from the whole team!</h5>
       </div>
-        
-      </div>
+
+    </div>
   </div>
 </template>
 
@@ -39,37 +39,40 @@ export default {
 
 <style lang="scss" scoped>
 @use '../assets/partials/var.scss' as *;
-.guest{
+
+.guest {
   width: 350px;
-  .card{
+
+  .card {
     background-color: white;
     color: $primary;
   }
-  .guest-star {
-      position: absolute;
-      right: 0;
-      top: 0;      margin-left: -50px;
-      z-index: 5;
-      width: 100px;
-      height: 100px;
-      border-radius: 50%;
-      overflow: hidden;
-      background: #ccc;
-      border: 3px solid #fff;
 
-      img {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        -webkit-transform: translate(-50%, -50%);
-        -moz-transform: translate(-50%, -50%);
-        -ms-transform: translate(-50%, -50%);
-        -o-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-        width: 100px;
-        height: auto;
-      }
+  .guest-star {
+    position: absolute;
+    right: 0;
+    top: 0;
+    margin-left: -50px;
+    z-index: 5;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    overflow: hidden;
+    background: #ccc;
+    border: 3px solid #fff;
+
+    img {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      -webkit-transform: translate(-50%, -50%);
+      -moz-transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+      -o-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      width: 100px;
+      height: auto;
+    }
   }
 }
-
 </style>
